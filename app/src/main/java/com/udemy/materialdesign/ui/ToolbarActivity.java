e202f926ab87df0c2be73f31461764f59e7fc839
flavio.santana <9090flavio@gmail.com>
2018-05-19 10:17:46 -0300

    Adicionando Click no Menu

--- app/src/main/java/com/udemy/materialdesign/ui/ToolbarActivity.java
+++ app/src/main/java/com/udemy/materialdesign/ui/ToolbarActivity.java
@@ -4,11 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.udemy.materialdesign.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnItemSelected;
 
 public class ToolbarActivity extends AppCompatActivity {
 
@@ -28,5 +31,30 @@ public class ToolbarActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_toolbar, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_buscar : {
+                Toast.makeText(ToolbarActivity.this, "Buscando item...", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.menu_carrinho  : {
+                Toast.makeText(ToolbarActivity.this, "Indo para carrinho...", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.menu_configuracao  : {
+                Toast.makeText(ToolbarActivity.this, "Indo para configuração...", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.menu_sobre  : {
+                Toast.makeText(ToolbarActivity.this, "Sobre nosso app...", Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
