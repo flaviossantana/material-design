62623b6cfdcc796ee75833f364029962f45d4720
flavio.santana <9090flavio@gmail.com>
2018-05-19 23:48:24 -0300

    Configurando a Navigation Drawer

--- app/src/main/java/com/udemy/materialdesign/ui/NavigationDrawerActivity.java
+++ app/src/main/java/com/udemy/materialdesign/ui/NavigationDrawerActivity.java
@@ -1,16 +1,24 @@
 package com.udemy.materialdesign.ui;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 import com.udemy.materialdesign.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class NavigationDrawerActivity extends AppCompatActivity {
 
+    @BindView(R.id.tolbar_nav_draw)
     public Toolbar toolbar;
+
+    @BindView(R.id.nav_DrawerLayout)
+    public DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,21 @@ public class NavigationDrawerActivity extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.abrir, R.string.fechar);
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return;
+        }
+
+        super.onBackPressed();
     }
 }
