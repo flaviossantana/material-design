5a441835e1c4b94e5250e9bc1b5f8588384e954e
flavio.santana <9090flavio@gmail.com>
2018-05-20 15:43:32 -0300

    Criando RadioButton e RadioGroup

--- app/src/main/java/com/udemy/materialdesign/ui/SelectionControlsActivity.java
+++ app/src/main/java/com/udemy/materialdesign/ui/SelectionControlsActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SwitchCompat;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.udemy.materialdesign.R;
@@ -11,6 +13,7 @@ import com.udemy.materialdesign.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnCheckedChanged;
+import butterknife.OnClick;
 
 public class SelectionControlsActivity extends AppCompatActivity {
 
@@ -34,5 +37,25 @@ public class SelectionControlsActivity extends AppCompatActivity {
         Toast.makeText(this, isChecked ? "Quero!": "Não Quero!", Toast.LENGTH_SHORT).show();
     }
 
+    @OnClick({R.id.sc_opcao_01, R.id.sc_opcao_02, R.id.sc_opcao_03})
+    public void onChangeCheckbox(RadioButton  radioButton){
+
+        boolean checked = radioButton.isChecked();
+
+        switch (radioButton.getId()){
+            case R.id.sc_opcao_01 : {
+                Toast.makeText(this, "OPÇÃO 01", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.sc_opcao_02 : {
+                Toast.makeText(this, "OPÇÃO 02", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.sc_opcao_03 : {
+                Toast.makeText(this, "OPÇÃO 03", Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+    }
 
 }
