d5c4d50e00f8d1784211612a0d05ef01217bad25
flavio.santana <9090flavio@gmail.com>
2018-05-20 20:53:17 -0300

    Criando Dialogs com Opções

--- app/src/main/java/com/udemy/materialdesign/ui/DialogsActivity.java
+++ app/src/main/java/com/udemy/materialdesign/ui/DialogsActivity.java
@@ -15,10 +15,20 @@ import butterknife.OnClick;
 
 public class DialogsActivity extends AppCompatActivity {
 
+    public AlertDialog alertDialog;
+    public AlertDialog alertDialogItens;
+    public AlertDialog alertDialogMultChoice;
+
     @BindView(R.id.dialog_btn_alert)
     public AppCompatButton btnDialog;
 
-    public AlertDialog alertDialog;
+    @BindView(R.id.dialog_btn_itens)
+    public AppCompatButton btnDialogItens;
+
+    @BindView(R.id.dialog_btn_mutiple_choice)
+    public AppCompatButton btnDialogMultiple;
+
+    private String[] items = {"OPÇÃO 1", "OPÇÃO 2", "OPÇÃO 3", "OPÇÃO 4"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +36,45 @@ public class DialogsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dialogs);
         ButterKnife.bind(this);
 
+        createDialog();
+        createDialogSingleChoice();
+        createDialogMultipleChoice();
+
+    }
+
+    private void createDialogSingleChoice() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Selecione uma opção:");
+        builder.setSingleChoiceItems(items, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(DialogsActivity.this, items[i], Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setPositiveButton("OK", null);
+        builder.setNegativeButton("CANCELAR", null);
+
+        alertDialogItens = builder.create();
+    }
+
+    private void createDialogMultipleChoice() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Selecione uma opção:");
+        builder.setMultiChoiceItems(items, null, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int idx, boolean isChecked) {
+                Toast.makeText(DialogsActivity.this, items[idx] + ":" + isChecked, Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setPositiveButton("OK", null);
+        builder.setNegativeButton("CANCELAR", null);
+
+        alertDialogMultChoice = builder.create();
+    }
+
+    private void createDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialog);
-        builder.setMessage("Deseja ver essa mensagem?");
+        builder.setTitle("Deseja ver essa mensagem?");
         builder.setPositiveButton("SIM", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -41,14 +88,22 @@ public class DialogsActivity extends AppCompatActivity {
             }
         });
 
-
         alertDialog = builder.create();
-
     }
 
     @OnClick(R.id.dialog_btn_alert)
     public void onClickDialog(){
         alertDialog.show();
+    }
+
+    @OnClick(R.id.dialog_btn_itens)
+    public void onClickDialogItens(){
+        alertDialogItens.show();
+    }
+
+    @OnClick(R.id.dialog_btn_mutiple_choice)
+    public void onClickDialogMultChoice(){
+        alertDialogMultChoice.show();
     }
 
 }
