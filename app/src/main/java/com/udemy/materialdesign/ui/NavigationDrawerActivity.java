7f062fa0723ae5d31d6797f08c27d053d88258fc
flavio.santana <9090flavio@gmail.com>
2018-05-20 00:25:28 -0300

    Configurando a Toolbar

--- app/src/main/java/com/udemy/materialdesign/ui/NavigationDrawerActivity.java
+++ app/src/main/java/com/udemy/materialdesign/ui/NavigationDrawerActivity.java
@@ -1,5 +1,6 @@
 package com.udemy.materialdesign.ui;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -60,6 +61,7 @@ public class NavigationDrawerActivity extends AppCompatActivity implements Navig
 
         switch (item.getItemId()){
             case R.id.menu_nav_inbox : {
+                goActivity(TextFieldActivity.class);
                 Toast.makeText(this, "Clicou no inbox", Toast.LENGTH_SHORT).show();
                 break;
             }
@@ -85,4 +87,10 @@ public class NavigationDrawerActivity extends AppCompatActivity implements Navig
 
         return true;
     }
+
+    private void goActivity(Class aClass) {
+        Intent irActivity = new Intent(NavigationDrawerActivity.this, aClass);
+        startActivity(irActivity);
+    }
+
 }
