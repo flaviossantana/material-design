c820b6abf0b6f02a0652ee3f93dd8d71de93de0d
flavio.santana <9090flavio@gmail.com>
2018-05-21 20:28:34 -0300

    Customizando a Snackbar

--- app/src/main/java/com/udemy/materialdesign/ui/SnackbarsToastsActivity.java
+++ app/src/main/java/com/udemy/materialdesign/ui/SnackbarsToastsActivity.java
@@ -1,10 +1,12 @@
 package com.udemy.materialdesign.ui;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.udemy.materialdesign.R;
 
@@ -31,13 +33,22 @@ public class SnackbarsToastsActivity extends AppCompatActivity {
     @OnClick(R.id.st_add_btn)
     public void onClickFloat(View view){
         final Snackbar snackbar = Snackbar.make(view, "CLICADO COM SUCESSO.", Snackbar.LENGTH_LONG);
-        snackbar.setAction("FECHAR", new View.OnClickListener() {
+        snackbar.setAction("FECHAR", onClickFecharSnack(snackbar));
+        View snackView = snackbar.getView();
+        snackView.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+        TextView snackAction = snackView.findViewById(android.support.design.R.id.snackbar_action);
+        snackAction.setTextColor(getResources().getColor(R.color.colorSecondaryDark));
+        snackbar.show();
+    }
+
+    @NonNull
+    private View.OnClickListener onClickFecharSnack(final Snackbar snackbar) {
+        return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 snackbar.dismiss();
             }
-        });
-        snackbar.show();
+        };
     }
 
 }
